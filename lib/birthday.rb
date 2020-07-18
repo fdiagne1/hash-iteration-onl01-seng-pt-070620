d862b8cbcc628c8dd9a8483788b2f17ce953d685
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-18 06:20:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,9 @@ def happy_birthday(birthday_kids)
 end
 end
 
+def age_appropriate_birthday(kids)
+  kids.each do |kids_name, age|
+    if age < 10
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+end
+end
